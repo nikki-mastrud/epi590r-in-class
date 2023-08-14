05be578dba370b87d376d0b03332f2fde62d6a54
Nikki Mastrud <nikki.mastrud@emory.edu>
2023-08-14 11:15:54 -0400

    loaded readr

--- R/clean-data-bad.R
+++ R/clean-data-bad.R
@@ -1,12 +1,21 @@
+#try to think of changes you might make
+#I mean first of all I would want to see comments but I don't know if that's the kind of thing you're looking for here.
+
 nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
 							 "id", "nsibs", "samp", "race_eth", "sex", "region",
 							 "income", "res_1980", "res_2002", "age_bir")
 
 library(tidyverse)
 setwd("~/Documents/Teaching/Emory/epi590r-in-class/data/raw/")
-nlsy <- read_csv("nlsy.csv",
+#Error in setwd: cannot change working directory
+	#I assume this is because you cannot change the wd within an R project?
+
+nlsy <- readr::read_csv("nlsy.csv",
 								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
 								 skip = 1, col_names = nlsy_cols)
+#Error could not find function "read_csv"
+#Assume this is because we don't have the package for it
+#changed code to "readr::read_csv" to load that package
 
 library(dplyr)
 nlsy <- nlsy |>
