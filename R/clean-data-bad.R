6b30ff0808fd8fe46578485727f3b98fcec94b41
Nikki Mastrud <nikki.mastrud@emory.edu>
2023-08-14 11:25:36 -0400

    comment my code

--- R/clean-data-bad.R
+++ R/clean-data-bad.R
@@ -6,20 +6,19 @@ nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
 							 "income", "res_1980", "res_2002", "age_bir")
 
 library(tidyverse)
+#got a weird error about conflicts when loading this package?
+
 setwd("~/Documents/Teaching/Emory/epi590r-in-class/data/raw/")
-#Error in setwd: cannot change working directory
-	#I assume this is because you cannot change the wd within an R project?
+#Error in setwd: cannot change working directory because that wd does not exist on my computer
 
 nlsy <- readr::read_csv("data/raw/nlsy.csv",
 								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
 								 skip = 1, col_names = nlsy_cols)
-#Error could not find function "read_csv"
-#Assume this is because we don't have the package for it
-#changed code to "readr::read_csv" to load that package
 
 #new error that nlsy.csv does not exist in the current wd
 #because it's in the "raw" folder
 #change file name to follow path "data/raw/nlsy.csv"
+#commit
 
 library(dplyr)
 nlsy <- nlsy |>
@@ -28,8 +27,14 @@ nlsy <- nlsy |>
 				 race_eth_cat = factor(race_eth, labels = c("Hispanic", "Black", "Non-Black, Non-Hispanic")),
 				 eyesight_cat = factor(eyesight, labels = c("Excellent", "Very good", "Good", "Fair", "Poor")),
 				 glasses_cat = factor(glasses, labels = c("No", "Yes")))
+#no apparent errors
 
 nlsy <- na.omit(nlsy)
+#remove missing values
+#no errors
 
 setwd("../clean/")
+#cannot change working directory error again
+#wouldn't this need to be data/clean, not just clean, since the working directory ends at epi590r-in-class?
+
 write_rds(nlsy, "nlsy-complete-cases.rds")
