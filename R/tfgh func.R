02ec3ccf3ddf3bdb0fe6e3efd2c777dc5b33197c
Nikki Mastrud <nikki.mastrud@emory.edu>
2023-08-15 14:44:02 -0400

    afternoon update

---
+++ R/tfgh func.R
@@ -0,0 +1,11 @@
+location1 = length(which(loc == "Arizona")),
+location2 = length(which(loc == "Texas")),
+location3 = length(which(loc == "California")),
+
+time1 = length(which(time <= 5)),
+time2 = length(which(time >5 & time <= 10)),
+
+dvsummarize <- function () {
+
+
+}
